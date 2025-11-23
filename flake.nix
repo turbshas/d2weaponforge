90769ec7757633e46f6e09ca2afdbd0a21256702
turbshas <turbshas@gmail.com>
2025-11-23 01:09:43 -0500

    Support automatic direnv

--- flake.nix
+++ flake.nix
@@ -3,19 +3,5 @@
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs";
     };
-    outputs = inputs@{ self, nixpkgs, ... }:
-    let
-        system = "x86_64-linux";
-        pkgs = import inputs.nixpkgs { inherit system; };
-    in
-    {
-        devShells.${system}.default = pkgs.mkShell {
-            packages = [
-                pkgs.nodejs_24
-            ];
-            shellHook = ''
-                zsh
-            '';
-        };
-    };
+    outputs = inputs@{ self, nixpkgs, ... }: (import ./shell.nix) { inherit nixpkgs; };
 }
---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ nixpkgs ? (import nixpkgs) {} }:
+let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+in
+{
+    devShells.${system}.default = pkgs.mkShell {
+        packages = [
+            pkgs.nodejs_24
+        ];
+    };
+}
